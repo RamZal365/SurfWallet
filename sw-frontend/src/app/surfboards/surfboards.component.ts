d0dfac8658ff18075fac1737a2ca7ed4f21721f3
juandiegorubio <juan.diego.rubio@estudiantat.upc.edu>
2024-02-14 19:53:34 +0100

    feat: Create Surfboard component files
    Component files are created, but they do nothing

--- sw-frontend/src/app/surfboards/surfboards.component.ts
+++ sw-frontend/src/app/surfboards/surfboards.component.ts
@@ -1,13 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
+import { ApiService } from '../api/api.service';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'surfboards',
   templateUrl: './surfboards.component.html',
   styleUrls: ['./surfboards.component.css'],
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ButtonComponent,
+    MatButtonModule,
+    HttpClientModule
+  ],
   providers: []
 })
 export class SurfboardsComponent {
+  title = "Surf Wallet"
 
+  surfboards: any[] = [];
+  wetsuits: any[] = [];
+  spots: any[] = [];
+
+  constructor(private apiService: ApiService){}
+
+  getSurfboards(){
+    this.apiService.list('surfboards').subscribe((data: any[]) => {
+      this.surfboards = data;
+      console.log(data);
+    });
+  }
 }
